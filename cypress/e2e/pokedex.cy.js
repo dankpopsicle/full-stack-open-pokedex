1a463f0670ffea04babbadde97d69b96130ff801
Adam Taber <altaber@gmail.com>
2023-03-20 15:18:30 -0500

    add another cypress test

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,7 +1,15 @@
 describe('Pokedex', function() {
-  it('front page can be opened', function() {
+  beforeEach(() => {
     cy.visit('http://localhost:8080')
+  })
+
+  it('front page can be opened', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('can navigate to a pokemon page', function() {
+    cy.get('a[href*="venusaur"]').click()
+    cy.contains('chlorophyll')
   })
 })
